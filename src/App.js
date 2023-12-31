aa8100340904bc5ab69ceb149458591c8b678136
Payalsasmal10 <sasmalpayal@gmail.com>
2023-12-31 18:48:11 +0530

    multiple data selection are on progress

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import data from "./data.json";
+import {useState} from "react";
 
 function App() {
+  const [languages, setLanguages] = useState(data);
+
+  const languageHandler = (index) =>{
+
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form className="App" action='#'>
+      <label>Choose the skills :</label>
+        {languages.map((item, index)=> {
+          return(
+            <div key={item.language}>
+              <input type='checkbox' onClick={()=>languageHandler(index)}/>
+              <label>{item.language}</label>
+            </div>
+          )
+        }
+        )}
+    </form>
   );
 }
 
